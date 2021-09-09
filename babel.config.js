c2d113d16a6cc1192e722d8d37fa211c4b88a797
Iryoda <iryodap@gmail.com>
2021-09-09 11:58:46 -0300

    :wrench: chore: config babel path

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,29 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+          alias: {
+            '@assets': './public/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@hooks': './src/hooks',
+            '@styles': './src/styles',
+            '@shared': './src/shared',
+            '@services': './src/services',
+            '@routes': './src/routes',
+            '@types': './src/types',
+            '@interfaces': './src/interfaces',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
