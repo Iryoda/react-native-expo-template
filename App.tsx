836b199f854963d85079bab3c4d73955775dfc53
Iryoda <iryodap@gmail.com>
2021-09-09 13:54:46 -0300

    :sparkles: feat: new Home page

--- App.tsx
+++ App.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components';
 import AppProvider from '@hooks/index';
-
-const Title = styled(Text)`
-  font-size: 24px;
-`;
+import Home from '@screens/Home';
 
 export default function App() {
   return (
     <AppProvider>
-      <View style={styles.container}>
-        <Title>Poli Junior</Title>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Home />
     </AppProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
