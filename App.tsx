426d67021e76c9b07c8e47a7134a49db73ba879b
Iryoda <iryodap@gmail.com>
2021-09-09 13:06:27 -0300

    :sparkles: feat: add styled-components themes

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components';
+import AppProvider from '@hooks/index';
 
 const Title = styled(Text)`
   font-size: 24px;
@@ -9,11 +10,13 @@ const Title = styled(Text)`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Title>Poli Junior</Title>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppProvider>
+      <View style={styles.container}>
+        <Title>Poli Junior</Title>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AppProvider>
   );
 }
 
