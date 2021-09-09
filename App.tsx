477f78115a48a7548a000c6b6df876be7b7ab785
Iryoda <iryodap@gmail.com>
2021-09-09 12:20:40 -0300

    :wrench: chore: add styled-components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Title = styled(Text)`
+  font-size: 24px;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Title>Poli Junior</Title>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
