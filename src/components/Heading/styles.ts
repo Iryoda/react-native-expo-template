c297084e302b8be1cdc07eae553a3d14836ffe80
Iryoda <iryodap@gmail.com>
2021-09-09 13:56:48 -0300

    :white_check_mark: test: add Heading test

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components/native';
 export const StyledHeading = styled(Text)<{ color?: string }>`
   ${({ theme, color }) => css`
     font-size: 32px;
-    margin-bottom: 40px;
+    margin-bottom: 24px;
     color: ${color || `${theme.colors.black}`};
   `}
 `;
